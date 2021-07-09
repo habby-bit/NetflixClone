b826619ecb0ed83e25baea0c5f223fa196ef888d
Habby Olusesi <habbyolu@gmail.com>
2021-07-09 18:08:02 -0400

    js switching tabs

--- js/main.js
+++ js/main.js
@@ -4,15 +4,29 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 // Select Tab Content Item
 function selectItem(e) {
     removeBorder();
-    
+    removeShow();
+
     // Add Border To Current Tab
     this.classList.add('tab-border');
+    
+    // Grab Content Item From DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+
+    // Add Show Class
+    tabContentItem.classList.add('show');
 }
 
 // Remove Border From Unclicked Tabs
 function removeBorder() {
     tabItems.forEach(item => {
         item.classList.remove('tab-border')
+    })
+}
+
+// Remove Content From Display Area
+function removeShow() {
+    tabContentItems.forEach(item => {
+        item.classList.remove('show')
     })
 }
 
